91631d559e38133cdd2fda8e7edd9b6906752519
iamkhushbooy <khushbooygkp@gmail.com>
2024-08-01 01:29:19 +0530

    added login

--- app/_components/Login.tsx
+++ app/_components/Login.tsx
@@ -5,20 +5,20 @@ const page = () => {
   const [password, setPassword] = useState('')
   return (
     
-      <div className='w-[80%] h-[60vh] bg-white
+      <div className='w-[90%] h-[300px] bg-white
        rounded flex justify-center items-center flex-col
         shadow-2xl'>
-        <input type="text" className='w-[70%] h-[8vh] rounded my-5 border px-2'
+        <input type="text" className='w-[90%] h-[8vh] rounded my-5 border px-2'
           placeholder='email'
           value={email}
           onChange={e => setEmail(e.target.value)}
         />
-        <input type="text" className='w-[70%] h-[8vh]  rounded my-5 border px-2'
+        <input type="text" className='w-[90%] h-[8vh]  rounded my-5 border px-2'
           placeholder='password'
           value={password}
           onChange={e => setPassword(e.target.value)} />
         <button className='text-blue-500 border border-blue-500 
-        px-2 py-1 font-bold w-[70%] h-[8vh]  rounded mt-6'>Login</button>
+        px-2 py-1 font-bold w-[90%] h-[8vh]  rounded mt-6'>Login</button>
       </div>
    
   )
